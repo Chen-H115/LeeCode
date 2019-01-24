cba509ee059a2ddb616a12ca9588dbe4302b7ff8
Chen-H115 <c8514h115@gmail.com>
2019-01-24 23:48:16 +0800

    383

---
+++ 383.py
@@ -0,0 +1,50 @@
+#S1 Runtime: 44 ms
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        def helper():
+            for i in set(ransomNote) & set(magazine):
+                if ransomNote.count(i) > magazine.count(i):
+                    return False
+            return True
+        return len(set(ransomNote) - set(magazine)) is 0 and helper()
+#S2 Runtime: 52 ms
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        if ransomNote == "":
+            return True
+        list = []
+        for i in ransomNote:
+            list.append(i)
+        ransomNote = list
+        while True:
+            if ransomNote.count(ransomNote[0]) > magazine.count(ransomNote[0]):
+                return False
+            x = ransomNote.count(ransomNote[0])
+            while x > 0:
+                x -= 1
+                ransomNote.remove(ransomNote[0])
+            if len(ransomNote) == 0:
+                break
+        return True
+#S3 Runtime: 184 ms
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        for i in ransomNote:
+            if ransomNote.count(i) > magazine.count(i):
+                return False
+        return True
