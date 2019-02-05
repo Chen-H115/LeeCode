f608d1bdce36495e790c2035a218877ec7f71ce1
Chen-H115 <c8514h115@gmail.com>
2019-02-05 23:30:33 +0800

    191

---
+++ 191/191.py
@@ -0,0 +1,12 @@
+#Rumtime: 20 ms
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        sum = 0
+        while n != 0:
+            sum += 1
+            n = n & (n - 1)
+        return sum
