acfed63f627365f0294d6410b339291c2a11a72d
Chen-H115 <c8514h115@gmail.com>
2019-02-15 00:14:15 +0800

    217

--- 217/217.py
+++ 217/217.py
@@ -1,3 +1,28 @@
+#Time Limit Exceeded
 class Solution:
     def containsDuplicate(self, nums: 'List[int]') -> 'bool':
-        return len(nums) != len(set(nums))
+        for i in nums:
+            if nums.count(i) >= 2:
+                return True
+        return False
+
+#Runtime: 68 ms
+class Solution:
+    def containsDuplicate(self, nums: 'List[int]') -> 'bool':
+        nums.sort()
+        for i in range(0, len(nums)):
+            if i + 1 in range(0, len(nums)) and nums[i] == nums[i + 1]:
+                return True
+        return False
+
+#Runtime: 52 ms
+class Solution:
+    def containsDuplicate(self, nums: 'List[int]') -> 'bool':
+        nums_dict = {}
+        for i in nums:
+            try:
+                nums_dict[i] +=1
+                return True
+            except:
+                nums_dict[i] = 1
+        return False
